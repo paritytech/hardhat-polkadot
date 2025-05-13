49fc298350aa786111e7fa3ef7c9af4ba2661934
Tiago Tavares <tiago.tavares@parity.io>
2025-05-13 16:18:48 +0100

    tests: fixes deployment value check

--- tests/e2e/4-deploy-live.test.sh
+++ tests/e2e/4-deploy-live.test.sh
@@ -8,15 +8,9 @@ cp ./simple-local-test-deploy.config.js ./lock/hardhat.config.js
 cd ./lock # relative to tmp folder
 npm add $HARDHAT_POLKADOT_TGZ_PATH
 npm install # install modules specified in the package.json
-npx hardhat node > hardhat-node.log 2>&1 & # Start the Hardhat node in the background
-HARDHAT_NODE_PID=$!
-while ! grep -q "Imported #5" hardhat-node.log; do  # Wait until producing blocks appears
-  sleep 0.2
-done
-trap "kill $HARDHAT_NODE_PID" EXIT
 
 # When
-yes | npx hardhat ignition deploy ./ignition/modules/Lock.js --network localNode > deploy.log 2>&1
+yes | npx hardhat ignition deploy ./ignition/modules/Lock.js --network westendHub > deploy.log 2>&1
 DEPLOY_LOCAL_NODE_OUTPUT=$(<deploy.log)
 
 # Then
@@ -24,4 +18,4 @@ assert_directory_not_empty "artifacts-pvm"
 assert_directory_not_empty "cache-pvm"
 check_log_value "$DEPLOY_LOCAL_NODE_OUTPUT" "LockModule#Lock - 0x"
 
-echo "Deploys on local node successfully in fixture-pojects/lock ✅"
+echo "Deploys on live network successfully in fixture-pojects/lock ✅"
