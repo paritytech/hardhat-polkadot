fcf33ed53a8e6f9435b300c5a2627bba22c3b6fc
Tiago Tavares <tiago.tavares@parity.io>
2025-05-13 15:27:28 +0100

    test: only runs cicd on macos

--- tests/e2e/1-compile.test.sh
+++ tests/e2e/1-compile.test.sh
@@ -10,7 +10,7 @@ npm add $HARDHAT_POLKADOT_TGZ_PATH
 npm install # install modules specified in the package.json
 
 # When
-run_test_and_handle_failure "npx hardhat compile" 0
+run_test_and_handle_failure "npx hardhat compile --show-stack-traces" 0
 
 # Then
 assert_directory_exists "artifacts-pvm"
--- tests/fixture-projects/basic-compile.config.js
+++ tests/fixture-projects/basic-compile.config.js
@@ -3,6 +3,7 @@ require("@parity/hardhat-polkadot")
 module.exports = {
     solidity: "0.8.28",
     resolc: {
+        version: "1.5.2",
         compilerSource: "npm",
     },
     networks: {
