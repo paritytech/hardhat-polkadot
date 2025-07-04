fb5f8c0f76dcfb6e874ef4f7033dc292fa60256e
bee344 <alberto.penayo@parity.io>
2025-07-04 09:47:28 -0300

    fix(hardhat-polkadot-resolc): updated types

--- packages/hardhat-polkadot-resolc/src/compile/index.ts
+++ packages/hardhat-polkadot-resolc/src/compile/index.ts
@@ -1,5 +1,4 @@
 import type { CompilerInput } from "hardhat/types"
-import chalk from "chalk"
 import type { SolcOutput } from "@parity/resolc"
 import type { ResolcConfig } from "../types"
 import { ResolcPluginError } from "../errors"
@@ -19,13 +18,6 @@ export async function compile(resolcConfig: ResolcConfig, input: CompilerInput) 
         }
         compiler = new BinaryCompiler(resolcConfig)
     } else if (resolcConfig.compilerSource === "npm") {
-        if (resolcConfig.settings?.batchSize)
-            console.warn(
-                chalk.yellow(
-                    "Batch compilation is only available for `binary` source.\nSetting batchSize will be ignored.",
-                ),
-            )
-
         compiler = new NpmCompiler(resolcConfig)
     } else {
         throw new ResolcPluginError(`Incorrect compiler source: ${resolcConfig.compilerSource}`)
