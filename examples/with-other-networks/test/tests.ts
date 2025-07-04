e6dd15b3a8853b8720a841764ea44578b863ee05
Alberto Nicolas Penayo <74352651+bee344@users.noreply.github.com>
2025-07-04 11:36:46 -0300

    fix(examples): add example on skipping test (#213)
    * fix(examples): add example on skipping test
    
    * cleaned up test

--- examples/with-other-networks/test/tests.ts
+++ examples/with-other-networks/test/tests.ts
@@ -2,6 +2,21 @@ import { expect } from 'chai';
 import hre from 'hardhat';
 
 describe('Greeter', function () {
+    const isPolkaVMCompatible = hre.network.polkavm;
+
+    // This is an example on how to skip tests that, due to incompatibilities,
+    // are currently not working with Polkadot.
+    // There are many other variations on how to do this.
+    it('Should run only on compatible PolkaVM chains', async function () {
+        if (!isPolkaVMCompatible) {
+            console.log(`Skipping test: not running on compatible PolkaVM chains.`);
+            this.skip();
+        }
+        const network = await hre.ethers.provider.getNetwork();
+
+        expect(network.chainId.toString().startsWith('420420')).to.be.true;
+    });
+
     it('Should set the greeting to the constructor argument', async function () {
         const greeting = 'Hello, world!';
 
