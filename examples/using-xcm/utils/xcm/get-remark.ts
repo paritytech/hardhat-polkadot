ddfbe0e7331f392d0b84e2627d215867ce2cca56
Tiago Bandeira <tiago.bandeira@parity.io>
2025-07-31 15:59:46 +0100

    fix: encoding the pallet + call indexes with the remark_with_event

--- examples/using-xcm/utils/xcm/get-remark.ts
+++ examples/using-xcm/utils/xcm/get-remark.ts
@@ -4,19 +4,24 @@ import {
     XcmV5Instruction,
     XcmVersionedXcm,
 } from "@polkadot-api/descriptors" // npm run postinstall
-import { Binary, getTypedCodecs } from "polkadot-api"
+import { Binary, createClient, getTypedCodecs } from "polkadot-api"
+import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat"
+import { getWsProvider } from "polkadot-api/ws-provider/web"
 
 export async function getRemark(remark: string): Promise<string> {
     const codecs = await getTypedCodecs(passetHub)
 
-    const remarkWithEventEncoded = codecs.tx.System.remark_with_event.enc({
+    const PAssetHubClient = createClient(withPolkadotSdkCompat(getWsProvider("https://testnet-passet-hub.polkadot.io")));
+    const PAssetHubApi = PAssetHubClient.getTypedApi(passetHub);
+
+    const remarkWithEventEncoded = await PAssetHubApi.tx.System.remark_with_event({
         remark: Binary.fromText(remark),
-    })
+    }).getEncodedData()
 
     const xcm = XcmVersionedXcm.V5([
         XcmV5Instruction.Transact({
             origin_kind: XcmV2OriginKind.Native(),
-            call: Binary.fromBytes(remarkWithEventEncoded),
+            call: remarkWithEventEncoded,
         }),
     ])
 
