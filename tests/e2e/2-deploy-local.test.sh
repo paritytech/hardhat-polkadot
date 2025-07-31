d7d8392f16bc5aceec4241dbbed88a8184a0dae2
Tiago Tavares <tiago.tavares@parity.io>
2025-07-31 15:38:35 +0100

    chore: echo node log during e2e test

--- tests/e2e/2-deploy-local.test.sh
+++ tests/e2e/2-deploy-local.test.sh
@@ -11,7 +11,8 @@ npm install # install modules specified in the package.json
 npx hardhat node > hardhat-node.log 2>&1 & # Start the Hardhat node in the background
 HARDHAT_NODE_PID=$!
 while ! grep -q "Imported #5" hardhat-node.log; do  # Wait until producing blocks appears
-  sleep 0.2
+  tail -n 5 hardhat-node.log
+  sleep 1
 done
 trap "kill $HARDHAT_NODE_PID" EXIT
 
