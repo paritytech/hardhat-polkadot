27ff6ba1969fc3093f00bea6eb898a39ab291e71
Tiago Tavares <tiago.tavares@parity.io>
2025-08-05 12:08:44 +0100

    fix: prevent rate limit during ci workflow

--- packages/hardhat-polkadot-resolc/src/download.ts
+++ packages/hardhat-polkadot-resolc/src/download.ts
@@ -8,6 +8,10 @@ import { CompilerName, type CompilerBuild, type CompilerList } from "./types"
 import { COMPILER_REPOSITORY_URL } from "./constants"
 
 const TEMP_FILE_PREFIX = "tmp-"
+const request_headers: Record<string, string> = {
+    Accept: "application/vnd.github+json",
+}
+if (process.env.GITHUB_TOKEN) request_headers.Authorization = `Bearer ${process.env.GITHUB_TOKEN}`
 
 function resolveTempFileName(filePath: string): string {
     const { dir, ext, name } = path.parse(filePath)
@@ -31,6 +35,7 @@ export async function download(
         const releasesResponse = await axios.get(url, {
             timeout: timeoutMillis,
             responseType: "json",
+            headers: request_headers,
         })
 
         const releasesData = releasesResponse.data
@@ -62,12 +67,6 @@ export async function download(
 
                 let sha256 = ""
                 if (!asset.digest || asset.digest == null) {
-                    console.log("platform", platform)
-                    console.log("name", name)
-                    console.log("COMPILER_REPOSITORY_URL", COMPILER_REPOSITORY_URL)
-                    console.log("version", version)
-                    console.log("asset", asset)
-
                     const checksumResponse = await axios.get(
                         `${COMPILER_REPOSITORY_URL}v${version}/checksums.txt`,
                         {
@@ -75,7 +74,6 @@ export async function download(
                             responseType: "text",
                         },
                     )
-                    console.log("checksumResponse", checksumResponse)
 
                     const tempFile = `./${TEMP_FILE_PREFIX}checksums.txt`
                     fsExtra.writeFileSync(tempFile, checksumResponse.data)
