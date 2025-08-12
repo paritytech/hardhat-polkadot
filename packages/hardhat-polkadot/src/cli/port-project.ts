e2479b1655a3c98b675cde3f6f69caf90446d4c6
Charles Hetterich <hetterich.charles@gmail.com>
2025-08-12 17:41:51 +0000

    first pass implementation of updating `package.json` of EVM project to be Polkadot compatible - ensure minimum version of `hardhat` - add `@parity/hardhat-polkadot` - We intentionally edit the package.json direclty as json, so we can stay agnostic to whatever PM tool is being used by a particular project, and keep the code pretty simple. - We're going to opt to error out on weird edge cases (if devDependencies doesn't exist, if `hardhat` isn't in there, etc. Because the `port` command should assume an existing functional EVM project— it's not our responsibility if their hardhat project is non-functional in the first place - we do track the indentation of the `package.json` so that the diff we create is minimal across almost all `package.json`s

--- packages/hardhat-polkadot/src/cli/port-project.ts
+++ packages/hardhat-polkadot/src/cli/port-project.ts
@@ -1,3 +1,34 @@
+import fs from "fs"
+import path from "path"
+
+const MIN_HARDHAT = "2.26.0" // TODO! don't hardcode this (get from peer dependency spec)
+
+// Detect indentation format of json file
+function detectIndent(src: string) {
+    const m = src.match(/\n([ \t]+)"/)
+    if (!m) return 2
+    const ws = m[1]
+    return ws.includes("\t") ? "\t" : ws.length
+}
+
 export function portProject(projectPath: string) {
-    console.log(`hello! porting @ ${projectPath}`)
+    // Fetch `package.json` object & formatting metadata
+    const pkgPath = path.join(projectPath, "package.json")
+    const raw = fs.readFileSync(pkgPath, "utf8")
+    const pkg = JSON.parse(raw) as any
+    const indent = detectIndent(raw)
+
+    // Ensure compatible `hardhat` version
+    const hardhatVersion = pkg.devDependencies.hardhat.match(/(\d+)\.(\d+)\.(\d+)/)[0]
+    console.log(hardhatVersion)
+    if (hardhatVersion < MIN_HARDHAT) {
+        console.log("updating `hardhat` version to comply with `@parity/hardhat-polkadot`")
+        pkg.devDependencies.hardhat = `^${MIN_HARDHAT}`
+    }
+
+    // Add `@parity/hardhat-polkadot` dependency
+    pkg.devDependencies["@parity/hardhat-polkadot"] = "^0.1.9" // TODO! get from package.json version
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, indent) + "\n", "utf8")
+
+    // TODO! Update `hardhat.config.*`
 }
