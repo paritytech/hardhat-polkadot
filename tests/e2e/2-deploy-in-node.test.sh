93c3f65849496e01705faac2d2cd86bdff7b1efe
Tiago Tavares <tiago.tavares@parity.io>
2025-08-12 17:14:50 +0100

    fix: add clean step in test

--- tests/e2e/2-deploy-in-node.test.sh
+++ tests/e2e/2-deploy-in-node.test.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
 set -e # Fail if any command fails
-. ./helpers.sh # import helpers functions inside tmp folder
+. ./helpers.sh  # import helpers functions inside tmp folder
 
 run_test() {
   # Given
   PROJECT_DIR=$1
   CONFIG_FILE=$2
   CONTRACT_NAME=$3
+  CHAIN_ID=$4
   cd "$TMP_TESTS_DIR/$PROJECT_DIR"
   cp "../$CONFIG_FILE" ./hardhat.config.js
   npm add "$HARDHAT_POLKADOT_TGZ_PATH"
@@ -21,10 +22,13 @@ run_test() {
   assert_directory_not_empty "artifacts-pvm"
   assert_directory_not_empty "cache-pvm"
   check_log_value "$DEPLOY_LOCAL_NODE_OUTPUT" "${CONTRACT_NAME}Module#${CONTRACT_NAME} - 0x"
-  npx hardhat ignition wipe chain-420420420 "${CONTRACT_NAME}Module#${CONTRACT_NAME}" # clean
-  
+
+  # Clean
+  npx hardhat ignition wipe chain-$CHAIN_ID "${CONTRACT_NAME}Module#${CONTRACT_NAME}"
+  kill "$HARDHAT_NODE_PID" >/dev/null 2>&1 || true
+  wait "$HARDHAT_NODE_PID" 2>/dev/null || true
   echo "Deploys on local node successfully in fixture-projects/${PROJECT_DIR} ✅"
 }
 
-# run_test "lock" "basic-test-and-deploy.config.js" "Lock"
-run_test "lock" "forking.config.js" "Lock"
+run_test "lock" "basic-test-and-deploy.config.js" "Lock" 420420420
+run_test "lock" "forking.config.js" "Lock" 420420422
