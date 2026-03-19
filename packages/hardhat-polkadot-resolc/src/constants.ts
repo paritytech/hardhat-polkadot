9e219e1e1c27ba76793d8dce0fb4a3fedd6056cd
Alberto Nicolas Penayo <74352651+bee344@users.noreply.github.com>
2026-03-19 18:44:18 -0300

    feat: hardhat 3 compat part 5 (#410)
    * feat: hardhat 3 compat part 5
    
    * cleanup
    
    * remove consts

--- packages/hardhat-polkadot-resolc/src/constants.ts
+++ packages/hardhat-polkadot-resolc/src/constants.ts
@@ -3,11 +3,6 @@ import type { ResolcConfig } from "./types.js"
 export const PLUGIN_NAME = "hardhat-polkadot"
 export const RESOLC_ARTIFACT_FORMAT_VERSION = "hh-resolc-artifact-1"
 export const DEFAULT_TIMEOUT_MILISECONDS = 30000
-export const TASK_COMPILE_SOLIDITY_GET_RESOLC_BUILD = "compile:solidity:resolc:get-build"
-export const TASK_COMPILE_SOLIDITY_COMPILE_RESOLC = "compile:solidity:resolc:compile"
-export const TASK_COMPILE_SOLIDITY_RUN_RESOLC = "compile:solidity:resolc:run"
-export const TASK_COMPILE_SOLIDITY_LOG_DOWNLOAD_RESOLC_COMPILER_START =
-    "compile:solidity:log:download-resolc-compiler-start"
 
 export const COMPILER_REPOSITORY_URL = "https://github.com/paritytech/revive/releases/download/"
 export const COMPILER_REPOSITORY_API_URL = "https://api.github.com/repos/paritytech/revive/releases"
